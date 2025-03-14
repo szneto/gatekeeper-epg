ea6e51504f8ad4e3c7e88fd9c51612df468262e5
Neto Souza <56355777+szneto@users.noreply.github.com>
2025-03-14 09:17:14 -0300

    Update json_to_xml.py

--- json_to_xml.py
+++ json_to_xml.py
@@ -5,7 +5,7 @@ import time
 import math
 
 def fetch_json():
-    url = "https://programacao.claro.com.br/gatekeeper/exibicao/select?q=id_revel:(1_2113+19_408+18_2214+20_2077+20_2091+28_1987+133_563+133_1004+133_858+133_545+133_1044+133_1420+133_1656+133_1292+319_1642+302_1177+161_2063+16_1868+16_1940+14_1899)&wt=json&rows=1000000&start=0&sort=id_canal+asc,dh_inicio+asc&fl=dh_fim dh_inicio st_titulo titulo id_programa id_canal id_cidade diretor elenco genero"
+    url = "https://programacao.claro.com.br/gatekeeper/exibicao/select?q=id_revel:(1_435+1_2113+19_408+18_2214+20_2077+20_2091+28_1987+133_1004+133_858+133_545+133_1044+133_1420+133_1656+133_1292+319_1642+302_1177+161_2063+16_1868+16_1940+14_1899)&wt=json&rows=1000000&start=0&sort=id_canal+asc,dh_inicio+asc&fl=dh_fim dh_inicio st_titulo titulo id_programa id_canal id_cidade diretor elenco genero"
     start_date = datetime.now(timezone.utc).date()
     end_date = start_date + timedelta(days=5)
     
@@ -56,7 +56,7 @@ def convert_to_xml(json_data):
     })
     
     channels = {
-        "563": "Globo-SP",
+        "435": "Globo-SP",
         "1004": "Record-SP",
         "858": "SBT-SP",
         "545": "Band-SP",
